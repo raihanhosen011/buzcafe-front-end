67aab019c6d6ae113a50a2f4497e976448124f92
Pranjal Agrawal <2018kucp1127@iiitkota.ac.in>
2022-01-18 22:17:16 +0000

    Tweaks

--- src/components/Transactions/TransactionCard.js
+++ src/components/Transactions/TransactionCard.js
@@ -5,15 +5,21 @@ export default function TransactionCard(props) {
         <div className='card shadow p-1 px-2 my-1 '>
             <div className='row'>
                 <div  className='col-2 my-auto'>
-                    <div style={{verticalAlign : 'middle'}} className='avatar text-center align-items-center '> <i style={{verticalAlign : 'middle'}} className={`fa fa-${props.mode == "RECEIVE" ? 'plus' : 'minus' }`} ></i></div>
+                    <div 
+                     style={{verticalAlign : 'middle'}}
+                     className={`avatar text-center align-items-center  ${props.mode == "RECEIVE" ? 'bg-success' : 'bg-danger' }`}> <i style={{verticalAlign : 'middle'}} className={`fa fa-${props.mode == "RECEIVE" ? 'plus' : 'minus' }`} ></i></div>
                 </div>
                 <div className='col-6'>
                 <h4>{props.mode == "RECEIVE" ? 'Receive from' : 'Paid to'}  </h4>
-                <p className='text-muted text-truncate'  tooltip={props.address} > {props.address}</p>
+                <p className='text-muted text-truncate'  data-toggle="tooltip" data-placement="top" title={props.address}> {props.address}</p>
                 </div>
 
-                <div className='col-4 my-auto'>
-                <h2 className={props.mode == "RECEIVE" ? 'text-success' : 'text-danger' }>{props.mode == "RECEIVE" ? '+' : '-' } { props.value} </h2>
+                <div className='col-4 my-auto text-right'>
+                    <h2 >
+                        {props.mode == "RECEIVE" ? '+' : '-' }
+                        { props.value}
+                        <img className="logo" src="https://s2.coinmarketcap.com/static/img/coins/200x200/4860.png" alt="ES"/>
+                    </h2>
                 </div>
             </div>
             <p className='text-small my-0 text-right'> <i className='fa fa-clock'></i> {props.time}</p>
--- src/components/Transactions/Transactions.js
+++ src/components/Transactions/Transactions.js
@@ -1,12 +1,13 @@
+import autoprefixer from 'autoprefixer'
 import React from 'react'
 import TransactionCard from './TransactionCard'
 
 export default function Transactions() {
     return (
-        <div style={style.transactionBox} className=' px-auto py-2 '>
+        <div style={style.transactionBox} className=' mx-auto py-2 '>
             <h1> Transactions </h1>
-            <TransactionCard mode="RECEIVE" address="0xC2A4EE96F1EaCE85aF86703B0E77A671F97EAC46" value="2522" time="2 days ago"/>
-            <TransactionCard mode="Send" address="0xC2A4EE96F1EaCE85aF86703B0E77A671F97EAC46" value="222" time="5 days ago"/>
+            <TransactionCard mode="RECEIVE" address="0xC2A4EE96F1EaCE85aF86703B0E77A671F97EAC46" value="2052200" time="2 days ago"/>
+            <TransactionCard mode="SEND" address="0xC2A4EE96F1EaCE85aF86703B0E77A671F97EAC46" value="222" time="5 days ago"/>
             <TransactionCard />
             <TransactionCard />
             <TransactionCard />
@@ -20,6 +21,7 @@ export default function Transactions() {
 
 const style = {
     transactionBox : {
-        maxWidth: '500px'
+        maxWidth: '1035px',
+        margin: 'auto'
     }
 }
